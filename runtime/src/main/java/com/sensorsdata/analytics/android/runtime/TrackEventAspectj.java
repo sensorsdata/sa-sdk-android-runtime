6efce6eed65be6afb034bb4ebb5bc8b00e9d9814
wangzhzh <wangzhuozhou@sensorsdata.cn>
2017-08-27 07:48:12 +0800

    Release 1.0.2

--- runtime/src/main/java/com/sensorsdata/analytics/android/runtime/TrackEventAspectj.java
+++ runtime/src/main/java/com/sensorsdata/analytics/android/runtime/TrackEventAspectj.java
@@ -1,20 +1,9 @@
 package com.sensorsdata.analytics.android.runtime;
 
-import android.text.TextUtils;
-
-import com.sensorsdata.analytics.android.sdk.SALog;
-import com.sensorsdata.analytics.android.sdk.SensorsDataAPI;
-import com.sensorsdata.analytics.android.sdk.SensorsDataThreadPool;
-import com.sensorsdata.analytics.android.sdk.SensorsDataTrackEvent;
-
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
-import org.json.JSONObject;
-
-import java.lang.reflect.Method;
 
 /**
  * Created by 王灼洲 on 2017/1/5
@@ -30,32 +19,6 @@ public class TrackEventAspectj {
 
     @After("methodAnnotatedWithTrackEvent()")
     public void trackEventAOP(final JoinPoint joinPoint) throws Throwable {
-        SensorsDataThreadPool.getInstance().execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-
-                    Method method = methodSignature.getMethod();
-                    SensorsDataTrackEvent trackEvent = method.getAnnotation(SensorsDataTrackEvent.class);
-                    String eventName = trackEvent.eventName();
-                    if (TextUtils.isEmpty(eventName)) {
-                        return;
-                    }
-
-                    String pString = trackEvent.properties();
-                    JSONObject properties = new JSONObject();
-                    if (!TextUtils.isEmpty(pString)) {
-                        properties = new JSONObject(pString);
-                    }
-
-                    SensorsDataAPI.sharedInstance().track(eventName, properties);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    SALog.i(TAG, "trackEventAOP error: " + e.getMessage());
-                }
-            }
-        });
-
+        AopUtil.sendTrackEventToSDK(joinPoint, "trackEventAOP");
     }
 }
